a8c5cd51b2889d747110c5e2f3e143479d18235d
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-01-10 22:03:39 -0300

    Criação do Projeto

---
+++ backend/src/main/java/com/OliveiraGusta/NovaFilmes/NovaFilmesApplication.java
@@ -0,0 +1,13 @@
+package com.OliveiraGusta.NovaFilmes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NovaFilmesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NovaFilmesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/OliveiraGusta/NovaFilmes/NovaFilmesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OliveiraGusta.NovaFilmes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NovaFilmesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
