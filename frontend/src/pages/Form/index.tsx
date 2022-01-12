5b8c5f74d08e17122798a6f2c8dee9458cbfc733
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-01-11 23:59:44 -0300

    Form layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,8 +1,42 @@
-function Form(){
+import "./style.css";
 
+function Form() {
 
-    return(
-<h1>Pagina da Form</h1>
+    const movie = {
+        id: 1,
+        image: "https://www.themoviedb.org/t/p/w533_and_h300_bestv2/jBJWaqoSCiARWtfV0GlqHrcdidd.jpg",
+        title: "The Witcher",
+        count: 2,
+        score: 4.5
+    };
+
+    return (
+        <div className="novafilmes-form-container">
+            <img className="novafilmes-movie-card-image" src={movie.image} alt={movie.title} />
+            <div className="novafilmes-card-bottom-container">
+                <h3>{movie.title}</h3>
+                <form className="novafilmes-form">
+                    <div className="form-group novafilmes-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group novafilmes-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="novafilmes-form-btn-container">
+                        <button type="submit" className="btn btn-primary novafilmes-btn">Salvar</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary novafilmes-btn mt-3">Cancelar</button>
+            </div >
+        </div >
 
     );
 }
