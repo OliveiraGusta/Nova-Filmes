1621b838ee8050bedb57fc35838d4a62ce1c5641
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-01-11 23:33:14 -0300

    Routes

---
+++ frontend/src/pages/Form/index.tsx
@@ -0,0 +1,9 @@
+function Form(){
+
+
+    return(
+<h1>Pagina da Form</h1>
+
+    );
+}
+export default Form
---
+++ frontend/src/pages/Listing/index.tsx
@@ -0,0 +1,9 @@
+function Listing(){
+
+
+    return(
+<h1>Pagina da listagem</h1>
+
+    );
+}
+export default Listing
