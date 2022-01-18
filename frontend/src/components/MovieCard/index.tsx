da6241bfc821a42a4fe69f0abeac8bbd46dd60ae
OliveiraGusta <gustavo.oliver456@gmail.com>
2022-01-17 22:08:55 -0300

    show score

--- frontend/src/components/MovieCard/index.tsx
+++ frontend/src/components/MovieCard/index.tsx
@@ -2,6 +2,7 @@ import MovieScore from "components/MovieScore";
 import { Link } from "react-router-dom";
 import { Movie } from "types/movie";
 
+
 type Props = {
     movie: Movie;
 }
@@ -15,7 +16,7 @@ function MovieCard({movie} : Props ) {
             <img className="novafilmes-movie-card-image" src={movie.image} alt={movie.title} />
             <div className="novafilmes-card-bottom-container">
                 <h3>{movie.title}</h3>
-                <MovieScore />
+                <MovieScore count={movie.count} score={movie.score}/>
 
                 <Link to={`/form/${movie.id}`}>
                     <div className="btn btn-primary novafilmes-btn">Avaliar</div>
